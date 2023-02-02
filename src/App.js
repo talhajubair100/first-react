dc69db0dc87f6e5f58679e3aa283101d8be86318
talhajubair100 <talhajubair100.bd@gmail.com>
2023-02-02 17:39:37 +0600

    All Components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Hero from './Components/Hero';
+import Clients from './Components/Clients';
+import About from './Components/About';
+import WhyUS from './Components/WhyUS';
+import Skill from './Components/Skill';
+import Service from './Components/Service';
+import Team from './Components/Team';
+import Pricing from './Components/Pricing';
+import Footer from './Components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Hero />
+      <Clients />
+      <About />
+      <WhyUS />
+      <Skill />
+      <Service />
+      <Team />
+      <Pricing />
+      <Footer />
+
     </div>
   );
 }
---
+++ src/Components/Skill.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Skill = () => {
+  return (
+    <div>
+      <section id="skills" class="skills">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-lg-6 d-flex align-items-center" data-aos="fade-right" data-aos-delay="100">
+            <img src="img/skills.png" class="img-fluid" alt="" />
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left" data-aos-delay="100">
+            <h3>Voluptatem dignissimos provident quasi corporis voluptates</h3>
+            <p class="fst-italic">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua.
+            </p>
+
+
+          </div>
+        </div>
+
+      </div>
+    </section>
+    </div>
+  )
+}
+
+export default Skill
